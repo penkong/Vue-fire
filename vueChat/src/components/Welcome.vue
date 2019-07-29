4c77333ea1f2174c84035ff6af9a27d88c8cec2b
mazdak <nazemi.works@gmail.com>
2019-07-29 06:43:37 -0700

    welcom comp

---
+++ vueChat/src/components/Welcome.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="welcome container">
+    <div class="card">
+      <div class="card-content center-align">
+        <h2 class="teal-text">Welcome</h2>
+        <form @submit.prevent="enterChat">
+          <label for="name">Enter Your Name:</label>
+          <input type="text" name="name" v-model="name" />
+          <button class="btn teal">Enter Chat</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Welcome",
+  data() {
+    return {
+      name: null
+    };
+  },
+  methods: {
+    enterChat() {
+      console.log(this.name);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.welcome {
+  max-width: 400px;
+  margin: 6rem;
+}
+.welcome h2 {
+  font-size: 2rem;
+}
+
+.welcome button {
+  margin: 30px auto;
+}
+</style>

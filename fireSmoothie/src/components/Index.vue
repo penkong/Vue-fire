e92eec618613c88f83dace5afe6666eb279ee7ce
mazdak <nazemi.works@gmail.com>
2019-07-29 00:22:45 -0700

    make usage of firebase fetch data and also delete it

--- fireSmoothie/src/components/Index.vue
+++ fireSmoothie/src/components/Index.vue
@@ -2,6 +2,7 @@
   <div class="index container">
     <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
       <div class="card-content">
+        <i class="material-icons delete" @click="deleteSmoothie(smoothie.id)">delete</i>
         <h2 class="indigo-text">{{ smoothie.title}}</h2>
         <ul class="ingredients">
           <li v-for="(ing, index) in smoothie.ingredients" :key="index">
@@ -14,26 +15,44 @@
 </template>
 
 <script>
+// bring db from firebase/init
+import db from "../firebase/init";
+
 export default {
   name: "Index",
   data() {
     return {
       // slug is part of url
-      smoothies: [
-        {
-          title: "Brew",
-          slug: "brew",
-          ingredients: ["bananas", "coffee", "milk"],
-          id: "1"
-        },
-        {
-          title: "Morning Mood",
-          slug: "morning-mood",
-          ingredients: ["mongo", "lime", "juice"],
-          id: "2"
-        }
-      ]
+      smoothies: []
     };
+  },
+  methods: {
+    deleteSmoothie(id) {
+      // delete doc from db
+      // for specific use .doc() for fetch all use .get()
+      db.collection("smoothies")
+        .doc(id)
+        .delete()
+        .then(
+          () =>
+            (this.smoothies = this.smoothies.filter(
+              smoothie => smoothie.id !== id
+            ))
+        );
+    }
+  },
+  created() {
+    db.collection("smoothies")
+      .get()
+      // refers state of smoothie collection at current point in time
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          // console.log(doc.data(), doc.id);
+          let smoothie = doc.data();
+          smoothie.id = doc.id;
+          this.smoothies.push(smoothie);
+        });
+      });
   }
 };
 </script>
@@ -59,5 +78,13 @@ export default {
 
 .index .ingredients li {
   display: inline-block;
+}
+.index .delete {
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  cursor: pointer;
+  color: #aaa;
+  font-size: 1.5rem;
 }
 </style>

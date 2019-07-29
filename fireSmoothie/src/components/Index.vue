d81c33741fe6a9fc18ae354bf081dd539fe92c08
mazdak <nazemi.works@gmail.com>
2019-07-28 22:04:32 -0700

    make card for smoothies

---
+++ fireSmoothie/src/components/Index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="index container">
+    <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
+      <div class="card-content">
+        <h2 class="indigo-text">{{ smoothie.title}}</h2>
+        <ul class="ingredients">
+          <li v-for="(ing, index) in smoothie.ingredients" :key="index">
+            <span class="chip">{{ ing }}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  data() {
+    return {
+      // slug is part of url
+      smoothies: [
+        {
+          title: "Brew",
+          slug: "brew",
+          ingredients: ["bananas", "coffee", "milk"],
+          id: "1"
+        },
+        {
+          title: "Morning Mood",
+          slug: "morning-mood",
+          ingredients: ["mongo", "lime", "juice"],
+          id: "2"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.index {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 0.8rem;
+  margin-top: 1rem;
+}
+
+.index h2 {
+  font-size: 1.5rem;
+  text-align: center;
+  margin-top: 0;
+}
+
+.index .ingredients {
+  margin: 30px auto;
+}
+
+.index .ingredients li {
+  display: inline-block;
+}
+</style>

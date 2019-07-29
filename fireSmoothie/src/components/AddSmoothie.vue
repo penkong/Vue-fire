3333886a8afc7cc051ce974d703eaa80237992df
mazdak <nazemi.works@gmail.com>
2019-07-29 01:27:39 -0700

    make app add to db and handle delete on creation

--- fireSmoothie/src/components/AddSmoothie.vue
+++ fireSmoothie/src/components/AddSmoothie.vue
@@ -6,11 +6,23 @@
         <label for="title">Smoothie</label>
         <input type="text" name="title" id="title" v-model="title" />
       </div>
+      <div class="field" v-for="(ing,index) in ingredients" :key="index">
+        <label for="ingredient">Ingredient:</label>
+        <input type="text" name="ingredient" v-model="ingredients[index]" />
+        <i class="material-icons delete" @click="deleteIng(ing)">delete</i>
+      </div>
       <div class="field add-ingredient">
         <label for="add-ingredient">Add an Ingredient</label>
-        <input type="text" name="add-ingredient" id="add-ingredient" />
+        <input
+          type="text"
+          name="add-ingredient"
+          id="add-ingredient"
+          @keydown.tab.prevent="addIng"
+          v-model="another"
+        />
       </div>
       <div class="field center-align">
+        <p v-if="feedback" class="red-text">{{ feedback }}</p>
         <button class="btn pink">Add Smoothie</button>
       </div>
     </form>
@@ -18,16 +30,56 @@
 </template>
 
 <script>
+import slugify from "slugify";
+import db from "../firebase/init";
 export default {
   name: "AddSmoothie",
   data() {
     return {
-      title: null
+      title: null,
+      another: null,
+      ingredients: [],
+      feedback: null,
+      slug: null
     };
   },
   methods: {
     AddSmoothie() {
-      console.log(this.title);
+      if (this.title) {
+        this.feedback = null;
+        // create a slug(url);
+        this.slug = slugify(this.title, {
+          replacement: "-",
+          remove: /[$*_+~.()'"!#%\-:@]/g,
+          lower: true
+        });
+        db.collection("smoothies")
+          .add({
+            title: this.title,
+            ingredients: this.ingredients,
+            slug: this.slug
+          })
+          .then(() => {
+            this.$router.push({ name: "Index" });
+          })
+          .catch(err => console.log(err));
+      } else {
+        this.feedback = "You must enter Smoothie title";
+      }
+    },
+    addIng() {
+      if (this.another) {
+        this.ingredients.push(this.another);
+        this.another = null;
+        this.feedback = null;
+      } else {
+        this.feedback = "You must enter a value";
+      }
+    },
+    deleteIng(ing) {
+      this.ingredients = this.ingredients.filter(
+        ingredient => ingredient !== ing
+      );
     }
   }
 };
@@ -47,5 +99,15 @@ export default {
 
 .add-smoothie .field {
   margin: 20px auto;
+  position: relative;
+}
+
+.add-smoothie .delete {
+  position: absolute;
+  right: 0;
+  bottom: 16px;
+  color: #aaa;
+  font-size: 1.4rem;
+  cursor: pointer;
 }
 </style>
